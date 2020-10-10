b2cf9f925625524005d3d21af3d200b84693bcb6
Hrishabh Joshi <joshi008@users.noreply.github.com>
2020-10-11 00:33:39 +0530

    just few updates_ addcourse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
-import Dash from './Screens/Dash';
+import React, { Suspense } from "react";
+import {
+	BrowserRouter,
+	Route,
+	Switch,
+	withRouter,
+	Redirect,
+} from "react-router-dom";
+import Login from "./Screens/authentication/Login/Login";
+import Signup from "./Screens/authentication/Signup/Signup";
+import Dash from "./Screens/Dasboard/Dash";
+import "./App.css";
+import AddCourse from "./Screens/AddCourse/AddCourse";
 
 function App() {
-  return (
-    <div className="App">
-      <Dash />
-    </div>
-  );
+	let routes = (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/login" render={(props) => <Login />} />
+				<Route path="/signup" render={(props) => <Signup />} />
+				<Route path="/dashboard" render={(props) => <Dash />} />
+				<Route path="/addcourse" render={(props) => <AddCourse />} />
+			</Switch>
+		</BrowserRouter>
+	);
+	return <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>;
 }
 
 export default App;
---
+++ frontend/src/Components/AddComp/AddComp.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import "./AddComp.css"
+import { Button } from '@material-ui/core';
+
+
+function AddComp() {
+
+    const inputProps = {
+        step: 300,
+    };
+
+    return (
+
+        <div className="add-app">
+            <h1>Add a new Roadmap and devote to social cause!</h1>
+
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+
+                id="name_project"
+                label="Name of Roadmap"
+                name="text"
+                autoComplete="name"
+                autoFocus
+            />
+
+            <Button
+                style={{
+                    borderRadius: 35,
+                    backgroundColor: "#21b6ae",
+                    padding: "9px 15px",
+                    fontSize: "18px"
+                }}
+                variant="contained"
+            >
+                Add Check-List
+            </Button>
+            <Button
+                style={{
+                    borderRadius: 35,
+                    backgroundColor: "#21b6ae",
+                    padding: "9px 15px",
+                    fontSize: "18px"
+                }}
+                variant="contained"
+            >
+                Add course
+            </Button>
+
+        </div>
+
+
+
+    )
+}
+
+export default AddComp
